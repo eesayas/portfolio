8b483d53e8db2e74d887cd41010e7830c2283da4
briones@ualberta.ca <briones@ualberta.ca>
2025-01-04 11:28:06 -0700

    update data

--- src/App.vue
+++ src/App.vue
@@ -1,116 +1,110 @@
 <template>
-<div id="app" class="d-flex h-100">
-  <div class="d-flex mx-auto">
-    <div class="d-table" style="padding-top: 6.5vw" id='left-panel'>
-      <About/><br>
-      <Skills/><br><br>
-      <Contact/>
-    </div>
-    
-    <div class="ml-5 h-100" id="projects">
-      <Project v-for="p in projects" :key="p.name" :data="{p}"/>
+  <div id="app" class="d-flex h-100">
+    <div class="d-flex mx-auto">
+      <div class="d-table" style="padding-top: 6.5vw" id="left-panel">
+        <About /><br />
+        <Contact />
+      </div>
+
+      <div class="ml-5 h-100" id="projects">
+        <Project v-for="p in projects" :key="p.name" :data="{ p }" />
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
-
-import About from "./components/About.vue"
-import Skills from "./components/Skills.vue"
-import Contact from "./components/Contact.vue"
-import Project from "./components/Project.vue"
-
+import About from "./components/About.vue";
+import Skills from "./components/Skills.vue";
+import Contact from "./components/Contact.vue";
+import Project from "./components/Project.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    About, Skills, Contact, Project
+    About,
+    Skills,
+    Contact,
+    Project,
   },
-  data(){
+  data() {
     return {
       projects: [
         {
-          langs: ['nestjs', 'nuxtjs', 'vuetify'],
-          name: 'PhilSA',
-          desc: "A portfolio/ecommerce website for the Philippine Students' Association at the University of Alberta",
+          langs: [
+            "VueJS",
+            "Pinia",
+            "Tailwind",
+            "Docker",
+            "MongoDB",
+            "NestJS",
+            "Turborepo",
+          ],
+          name: "Classie",
+          desc: "A classroom management software currently in use at a KUMON center in Edmonton.",
+          code: "",
+          app: "https://www.youtube.com/watch?v=C9_weFglsAU",
+        },
+        {
+          langs: ["webrtc", "preact", "deno", "websocket"],
+          name: "Confer",
+          desc: "A simple video conferencing app.",
+          code: "https://github.com/eesayas/iseeya",
+          app: "https://iseeya.eesayas.com/",
+        },
+        {
+          langs: ["python", "tensorflow"],
+          name: "Object Detection Shell",
+          desc: "A shell made with Python for training and testing Tensorflow models",
+          code: "https://github.com/eesayas/tensorflow-object-detection-shell",
+        },
+        {
+          langs: ["svelte"],
+          name: "Isaias & Chalsea",
+          desc: "A journal web app where you can say your thoughts and have it in writing.",
+          code: "https://github.com/eesayas/dear-diary",
+          app: "https://dear-diary.online/",
+        },
+        {
+          langs: ["nestjs", "nuxtjs", "vuetify"],
+          name: "PhilSA",
+          desc: "A corporate website created for the Philippine Students' Association at the University of Alberta",
           app: "https://philsa.ca/",
         },
         {
-          langs: ['webrtc', 'peerjs'],
-          name: 'iSeeYa',
-          desc: 'A simple video conferencing app that uses PeerJS and WebRTC.',
-          code: 'https://github.com/eesayas/iseeya',
-          app: "https://iseeya.eesayas.com/",
-        },
-        { 
-          langs: ['reactjs', 'mongodb', 'opencv'],
-          name: 'Classie',
-          desc: 'A ReactJS web application for student and staff management of a KUMON centre.',
-          code: '',
-          app: "https://www.youtube.com/watch?v=C9_weFglsAU",
-        },
-
-        {
-          langs: ['python', 'django', 'webscrapping'],
-          name: 'Gander',
-          desc: 'A Django web app for deciding what Netflix movie to watch with only a limited time.',
-          code: 'https://github.com/eesayas/Gander',
-          app: "https://gander-netflix.herokuapp.com/",
-        },
-
-        {
-          langs: ['wit.ai', 'speech recognition', 'speech-to-text'],
-          name: 'Dear Diary',
-          desc: 'A journal web app where you can say your thoughts and have it in writing.',
-          code: 'https://github.com/eesayas/dear-diary',
+          langs: ["wit.ai", "speech recognition", "speech-to-text"],
+          name: "Dear Diary",
+          desc: "A journal web app where you can say your thoughts and have it in writing.",
+          code: "https://github.com/eesayas/dear-diary",
           app: "https://dear-diary.online/",
         },
-
         {
-          langs: ['java', 'android', 'firebase'],
-          name: 'Mooood',
-          desc: 'An Android social media, mobile app for sharing your moods.',
-          code: 'https://github.com/eesayas/Mooood',
-          app: "",
-        },
-
-        {
-          langs: ['chrome extension', 'javascript'],
-          name: 'Watch Your Profanity',
-          desc: 'A google chrome extension for filtering f-words from Netflix.',
-          code: 'https://github.com/eesayas/watch-your-profanity',
-          app: "https://www.youtube.com/watch?v=ONhTewFSVVo",
-        },
-
-
-        {
-          langs: ['expressjs', 'googleapis', 'webhooks'],
-          name: 'reformationedmonton.ca',
-          desc: 'A website for a local church with auto notifications via gmail and webhooks.',
-          code: 'https://github.com/eesayas/reformationedmonton.ca',
+          langs: ["expressjs", "googleapis", "webhooks"],
+          name: "reformationedmonton.ca",
+          desc: "A website for a local church with auto notifications via gmail and webhooks.",
+          code: "https://github.com/eesayas/reformationedmonton.ca",
           app: "https://www.reformationedmonton.ca/",
         },
 
         {
-          langs: ['tailwindcss', 'expressjs'],
-          name: 'hopebargainshoppe.online',
-          desc: 'A booking website for customers of Hope Bargain Shoppe.',
-          code: 'https://github.com/eesayas/hopebargain',
+          langs: ["tailwindcss", "expressjs"],
+          name: "hopebargainshoppe.online",
+          desc: "A booking website for customers of Hope Bargain Shoppe.",
+          code: "https://github.com/eesayas/hopebargain",
           app: "https://hopebargainshoppe.online/",
         },
 
         {
-          langs: ['reactjs'],
-          name: 'Gomoku Online',
-          desc: 'A web game app for Gomoku, an abstract strategy board game. (Worked with a group of 3)',
-          code: 'https://github.com/ChrisChrisLoLo/GomokuOnline',
+          langs: ["reactjs"],
+          name: "Gomoku Online",
+          desc: "A web game app for Gomoku, an abstract strategy board game. (Worked with a group of 3)",
+          code: "https://github.com/ChrisChrisLoLo/GomokuOnline",
           app: "https://chrischrislolo.github.io/GomokuOnline/",
         },
-      ]
-    }
-  }
-}
+      ],
+    };
+  },
+};
 </script>
 
 <style>
@@ -124,52 +118,52 @@ export default {
   src: url("./fonts/Rubik-Medium.ttf");
 }
 
-@font-face{
+@font-face {
   font-family: "RubikLight";
-  src: url("./fonts/Rubik-Light.ttf")
+  src: url("./fonts/Rubik-Light.ttf");
 }
 
-#projects{
-  padding-top: 6vw; 
+#projects {
+  padding-top: 6vw;
   overflow-y: scroll;
 }
 
 /* Hide scrollbar for Chrome, Safari and Opera */
 #projects::-webkit-scrollbar {
-    display: none;
+  display: none;
 }
 
 /* Hide scrollbar for IE, Edge and Firefox */
 #projects {
-  -ms-overflow-style: none;  /* IE and Edge */
-  scrollbar-width: none;  /* Firefox */
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
 }
 
 @media screen and (max-width: 1250px) {
-  #projects{
+  #projects {
     margin-left: 1rem !important;
   }
 
-  #app{
+  #app {
     padding: 0 2rem;
   }
 }
 
 @media screen and (max-width: 845px) {
-  #left-panel{
+  #left-panel {
     background-color: #151515;
     width: 100%;
   }
 
-  #projects{
+  #projects {
     margin-left: 0 !important;
   }
 
-  #app{
+  #app {
     height: auto !important;
   }
 
-  #app > div{
+  #app > div {
     flex-direction: column;
   }
 }
--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,71 +1,89 @@
 <template>
-<div id="project" style="padding: 2rem 3rem;" role="button">
+  <div id="project" style="padding: 2rem 3rem" role="button">
     <div class="d-flex">
-        <span v-for="l in data.p.langs" :key="l" class="langs">{{l}}</span>
+      <span v-for="l in data.p.langs" :key="l" class="langs">
+        <img src="https://preactjs.com/favicon.ico" style="width: 1.5rem" />
+
+        {{ l }}
+      </span>
     </div>
-    <h2>{{data.p.name}}</h2>
-    <p>{{data.p.desc}}</p>
+    <h2>{{ data.p.name }}</h2>
+    <p>{{ data.p.desc }}</p>
 
     <div class="d-flex">
-        <a class="d-flex align-items-center mr-2" role="button" target="_blank" :href="data.p.code" v-if="data.p.code">
-            <font-awesome-icon :icon="['fas', 'code']"/>
-            <span class="mx-2">Code</span>
-        </a>   
+      <a
+        class="d-flex align-items-center mr-2"
+        role="button"
+        target="_blank"
+        :href="data.p.code"
+        v-if="data.p.code"
+      >
+        <font-awesome-icon :icon="['fas', 'code']" />
+        <span class="mx-2">Code</span>
+      </a>
 
-        <a class="d-flex align-items-center" role="button" target="_blank" :href="data.p.app" v-if="data.p.app">
-            <font-awesome-icon :icon="['fas', 'desktop']"/>
-            <span class="mx-2">View App</span>
-        </a>  
-    </div> 
-    
-</div>
+      <a
+        class="d-flex align-items-center"
+        role="button"
+        target="_blank"
+        :href="data.p.app"
+        v-if="data.p.app"
+      >
+        <font-awesome-icon :icon="['fas', 'desktop']" />
+        <span class="mx-2">View App</span>
+      </a>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "Project",
-    props: {
-        data: Object
-    }
-}
+  name: "Project",
+  props: {
+    data: Object,
+  },
+};
 </script>
 
 <style>
-#project{
-    background: #202022;
-    margin: 1rem 0;
-    user-select: none;
-    transition: all .3s ease-in-out;
+#project {
+  background: #202022;
+  margin: 1rem 0;
+  user-select: none;
+  transition: all 0.3s ease-in-out;
 }
 
-#project:hover{
-    box-shadow: rgba(0, 0, 0, 0.22) 0px 19px 43px;
-    transform: translate3d(0px, -7px, 0px);
+#project:hover {
+  box-shadow: rgba(0, 0, 0, 0.22) 0px 19px 43px;
+  transform: translate3d(0px, -7px, 0px);
 }
 
-span{
-    margin-left: 0.5rem;
-    font-family: RubikLight;
+span {
+  margin-left: 0.5rem;
+  font-family: RubikLight;
 }
 
-.langs{
-    text-transform: uppercase;
+.langs {
+  text-transform: uppercase;
+  align-items: center;
 }
 
-span:nth-child(1){
-    margin-left: 0;
+span:nth-child(1) {
+  margin-left: 0;
 }
 
-h2{
-    font-family: RubikMedium;
+h2 {
+  font-family: RubikMedium;
 }
 
-p{
-    color: #939393;
-    font-family: RubikLight;
+p {
+  color: #939393;
+  font-family: RubikLight;
+  flex-wrap: wrap;
+  display: flex;
 }
 
-a{
-    color: white !important;
+a {
+  color: white !important;
 }
 </style>
